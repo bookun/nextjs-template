a4236ce9c8d3572d34f15369e9f0437b0e06cd3a
bookun <kutsuzawa2851@gmail.com>
2020-09-13 23:20:48 +0900

    template

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { AppProps } from "next/app";
+import { FunctionComponent } from "react";
+
+const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from "react";
+
+type Props = unknown;
+const Home: FunctionComponent<Props> = () => {
+  return <div>hello nextjs</div>;
+};
+
+export default Home;
